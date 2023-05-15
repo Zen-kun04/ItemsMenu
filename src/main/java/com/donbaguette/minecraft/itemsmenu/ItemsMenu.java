414cbb5185d2b7e85643aea0a6057bb9756710df
Baguette <veryveroniquevery@gmail.com>
2023-05-16 00:25:29 +0200

    Created the config manager (for configs that are included in resources)

--- src/main/java/com/donbaguette/minecraft/itemsmenu/ItemsMenu.java
+++ src/main/java/com/donbaguette/minecraft/itemsmenu/ItemsMenu.java
@@ -14,34 +14,30 @@ public final class ItemsMenu extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         ConfigType configType = new ConfigType();
-
-        System.out.println("Testing 1");
         this.saveDefaultConfig();
         FileConfiguration config = this.getConfig();
         System.out.println(config.get("test1.test2.name"));
-        config.addDefault("Prueba1", "chi uwu");
-        config.addDefault("Prueba2", "jijijija");
-        config.addDefault("gay", true);
-        config.addDefault("test1.pene", false);
+        config.addDefault("TestingOne", "Yes");
+        config.addDefault("TestingTwo", "No");
+        config.addDefault("Yes", true);
+        config.addDefault("test1.False", false);
         config.options().copyDefaults(true);
         saveConfig();
+        /*
         configType.setConfigPath(this.getDataFolder().getPath());
         configType.setConfigName("TestingXD.yml");
         ConfigManager configManager = new ConfigManager(this, configType);
         try {
-            configManager.saveDefaultConfig();
+             configManager.saveDefaultConfig();
             FileConfiguration newConfig = configManager.getConfig();
-            newConfig.addDefault("NombreCompleto", "putakeriko");
+            newConfig.addDefault("Key", "Value");
             configManager.saveConfig();
 
 
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            System.out.println("no se pudo guardar la config");
             throw new RuntimeException(e);
         }
-
+        */
 
     }
 
